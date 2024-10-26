fed75aa288810acd1e04a029b10f05ff067be03f
Kerwin <kerwinmondesir@outlook.com>
2024-10-26 00:49:26 -0300

    Completed

--- part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -5,6 +5,24 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Give points [0-100]");
+        int grades = scan.nextInt();
+        if(grades >= 0 && grades <= 49){
+            System.out.println("Grade: failed");
+        } else if (grades >= 50 && grades <= 59){
+            System.out.println("Grade: 1");
+        } else if (grades >= 60 && grades <= 69){
+            System.out.println("Grade: 2");
+        } else if (grades >= 70 && grades <= 79){
+            System.out.println("Grade: 3");
+        } else if (grades >= 80 && grades <= 89){
+            System.out.println("Grade: 4");
+        } else if (grades >= 90 && grades <= 100) {
+            System.out.println("Grade: 5");
+        } else if (grades > 100){
+            System.out.println("Grade: incredible");
+        } else {
+            System.out.println("Grade: impossible!");
+        }
     }
 }
--- part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
+++ part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
@@ -7,6 +7,13 @@ public class OddOrEven {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here 
+        System.out.println("Give a number:");
+        int num = scan.nextInt();
+        if(num % 2 == 0){
+            System.out.println("Number " + num + " is even.");
+        } else {
+            System.out.println("Number " + num + " is odd.");
+        }
         // HINT:
         // You can find out if a number is even or odd easily using the modulo operator %
         // Try the following commands to see what they print
