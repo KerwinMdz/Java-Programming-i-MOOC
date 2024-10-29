6fa9195e0840568004ed6700a3847e809f6c9bca
Kerwin <kerwinmondesir@outlook.com>
2024-10-29 04:00:33 -0300

    Completed Part02

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,15 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Last number?");
+        int lastNum = scanner.nextInt();
+
+        int result = 0;
+        for(int i = 0; i < lastNum; i++){
+            result += i;
+            result++;
+        }
+        System.out.println("The sum is " + result);
 
     }
 }
