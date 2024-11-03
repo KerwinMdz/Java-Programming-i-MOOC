a050294ce1333094120a422571cc6ebbb85ef63f
Kerwin <kerwinmondesir@outlook.com>
2024-11-02 22:46:33 -0300

    Completed Part03

--- part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -6,11 +6,23 @@ public class AverageOfAList {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer>numbers = new ArrayList<>();
 
         // implement here a program, that first reads user input
         // adding them on a list until user gives -1.
+        while (true) {
+            int input = scanner.nextInt();
+            if(input == -1){
+                break;
+            }
+            numbers.add(input);
+        }
         // Then it computes the average of the numbers on the list
         // and prints it.
-        
+        int sum = 0;
+        for(Integer num : numbers){
+            sum += num;
+        }
+        System.out.println("Average: " + (double) sum / numbers.size());
     }
 }
