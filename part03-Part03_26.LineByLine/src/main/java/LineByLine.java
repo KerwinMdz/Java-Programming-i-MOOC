2de0dc4c6ecc5f57b57bc9802909e9d48b871925
Kerwin <kerwinmondesir@outlook.com>
2024-11-11 02:20:21 -0400

    Completed Part03

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,18 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){ 
+        System.out.print("");
+        String input = scanner.nextLine();
+        if(input.equals("")){
+            break;
+        }
+        String [] splitInput = input.split(" ");
+        for(int i = 0; i < splitInput.length; i++){
+            System.out.println(splitInput[i]);
+        } 
+    }
+        
 
     }
 }
