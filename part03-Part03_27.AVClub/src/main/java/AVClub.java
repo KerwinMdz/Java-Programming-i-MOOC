85378485b9b11c5b24c33987a3d9375ef5dfffe5
Kerwin <kerwinmondesir@outlook.com>
2024-11-12 22:53:25 -0400

    Completed Part03

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,6 +5,15 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            if(input.contains("av")){
+                System.out.println(input);
+            }
+        }
 
 
     }
