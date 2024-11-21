1af8c88f727933389171ce9a33d76f1aba520a80
Kerwin <kerwinmondesir@outlook.com>
2024-11-20 23:58:15 -0400

    Part03

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,20 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int outPut = 0;
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            String [] ageOfOldest = input.split(",");
+            int age = Integer.valueOf(ageOfOldest[1]);
+            if(age > outPut){
+                outPut = age;
+            }
+            
+        }
+        System.out.println("Age of the oldest: " + outPut);
 
     }
 }
