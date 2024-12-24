d58a753408d4896a145cf1248a07e476b0ee07b3
Kerwin <kerwinmondesir@outlook.com>
2024-12-23 22:16:15 -0400

    Completed Part03

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,28 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int sum = 0;
+        double average = 0;
+        int count = 0;
+        String longestName = "";
+        //int year = 0;
+        while(true){
+            String yearInput = scanner.nextLine();
+            if(yearInput.equals("")){
+                break;
+            }
+            count++;
+            String[] yearOutput = yearInput.split(",");
+            int outPut = Integer.valueOf(yearOutput[1]);
+            String name = yearOutput[0];
+            if(name.length() > longestName.length()){
+                longestName = name;
+            }
+            sum+=outPut;
+            average = (double)sum / count;
+            
+        }
+        System.out.println("Longest name:" + longestName);
+        System.out.println("Average of the birth years: " + average);
     }
 }
