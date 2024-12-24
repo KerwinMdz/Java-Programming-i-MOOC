b47e8625028d06e5a12e42ac1e61ceef01bfea05
Kerwin <kerwinmondesir@outlook.com>
2024-12-24 00:15:24 -0400

    Completed Part04

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,9 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account kerwinAccount = new Account("Kerwin's Account", 100.00);
+        kerwinAccount.deposit(20.00);
+
+        System.out.println(kerwinAccount);
     }
 }
