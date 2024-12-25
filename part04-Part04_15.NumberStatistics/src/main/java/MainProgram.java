3d3aae80b5491f0b8cc2f23f010d90a2aa868795
Kerwin <kerwinmondesir@outlook.com>
2024-12-25 00:17:25 -0400

    Part04 Completed

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,24 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        Statistics stats = new Statistics();
+        Statistics sumEven = new Statistics();
+        Statistics sumOdd = new Statistics();
+        System.out.println("Enter numbers: ");
+        while(true){
+            int input = scanner.nextInt();
+            if(input == -1){
+                break;
+            }
+            if(input % 2 == 0){
+                sumEven.addNumber(input);
+            } else {
+                sumOdd.addNumber(input);
+            }
+            stats.addNumber(input);
+        }
+        System.out.println("Sum: " + stats.sum());
+        System.out.println("Sum of even numbers: " + sumEven.sum());
+        System.out.println("Sum of odd numbers: " + sumOdd.sum());
     }
 }
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,28 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+
+
+    public Statistics(){
+    }
+
+    public void addNumber(int number){
+        //Can remain empty for now
+        this.sum+=number;
+        this.count = this.count + 1;
+    }
+    public int getCount(){
+        return this.count;
+    }
+    public int sum(){
+        return sum;
+    }
+    public double average(){
+        
+        if(this.sum != 0 && this.count != 0){
+        return (double) this.sum / this.count;
+    }
+    return 0.0;
+    }
 }
