202b6702cd0e46948e7b0151904e4c84b7c52a04
Kerwin <kerwinmondesir@outlook.com>
2024-12-26 02:08:09 -0400

    Completed Part04

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,35 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        Scanner kbd = new Scanner(System.in);
+        ArrayList<Book>books = new ArrayList<>();
+        while(true){
+            System.out.println("Title: ");
+            String bookTitle = kbd.nextLine();
 
+            if(bookTitle.equals("")){
+                break;
+            }
+
+            System.out.println("Pages: ");
+            int bookPages = kbd.nextInt();
+
+            System.out.println("Publication year: ");
+            int bookPubYr = kbd.nextInt();
+
+            kbd.nextLine();
+
+            books.add(new Book(bookTitle, bookPages, bookPubYr));
+        }
+        System.out.println("What information will be printed?");
+        String toPrint = kbd.next();
+        for(int i = 0; i < books.size(); i++){
+            Book bookz = books.get(i);
+            if(toPrint.equals("everything")){
+                System.out.println(bookz);
+            } else if(toPrint.equals("name")){
+                System.out.println(bookz.getTitle());
+            }
+        }
     }
 }
