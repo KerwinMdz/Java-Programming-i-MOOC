05d37f95f46af6278b82377bd392088fca9a6d7a
Kerwin <kerwinmondesir@outlook.com>
2024-12-28 00:06:45 -0400

    Completed Part04

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,11 +1,13 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
         System.out.print("File? ");
         String file = scanner.nextLine();
@@ -14,6 +16,21 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        //convert to int
+    
+        int count = 0;
+        try(Scanner fileReader = new Scanner(Paths.get(file))){
+            while(fileReader.hasNextLine()){
+                //list.add(fileReader.nextLine());
+                int row = Integer.valueOf(fileReader.nextLine());
+                if(row >= lowerBound && row <= upperBound){
+                    count++;
+            }
+            }
+            System.out.println("Numbers: " + count);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -7,5 +7,30 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        
+        try{
+            Scanner fileReader = new Scanner(Paths.get(file));
+            while (fileReader.hasNextLine()) {
+                String row = fileReader.nextLine();
+                String [] splitName = row.split(",");
+                String names = splitName[0];
+                int ages = Integer.valueOf(splitName[1]);
+                
+                if(ages ==1){
+                    System.out.println(names + ", age: " + ages + " year");
+                } else
+                System.out.println(names + ", age: " + ages + " years");
+                     
+               
+
+            }
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+
     }
 }
