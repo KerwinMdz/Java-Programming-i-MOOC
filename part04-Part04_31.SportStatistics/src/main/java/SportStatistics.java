a4c4f21f6cb56671095bd83e4b7a24549c20855e
Kerwin <kerwinmondesir@outlook.com>
2024-12-29 23:14:06 -0400

    Completed Part04

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,6 +7,46 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("File: ");
+        String file = scan.nextLine();
+
+        System.out.println("Team: ");
+        String teamInput = scan.nextLine();
+        int count = 0;
+        int win = 0;
+        int loss = 0;
+        try (Scanner fileReader = new Scanner(Paths.get(file))){
+            while(fileReader.hasNextLine()){
+               
+                String row = fileReader.nextLine();
+                String [] splitRow = row.split(",");
+
+                String hometeam = splitRow[0];
+                String visitingTeam = splitRow[1];
+
+                int homeTeamPoints = Integer.valueOf(splitRow[2]);
+                int visitingTeamPoints = Integer.valueOf(splitRow[3]);
+
+                if (hometeam.equals(teamInput) || visitingTeam.equals(teamInput)) {
+                    count++;
+
+                if (hometeam.equals(teamInput) && homeTeamPoints > visitingTeamPoints) {
+                    win++;
+                } else if (visitingTeam.equals(teamInput) && visitingTeamPoints > homeTeamPoints) {
+                    win++;
+                } else {
+                    loss++;
+                }
+
+            }
+            }
+            System.out.println("Games: " + count);
+            System.out.println("Wins: " + win);
+            System.out.println("Losses: " + loss);
+            
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 
