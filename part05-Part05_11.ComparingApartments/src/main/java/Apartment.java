1043ff13c162a1417885bb5928f6a0b9de134d49
Kerwin <kerwinmondesir@outlook.com>
2025-02-18 00:13:30 -0400

    Completed Part05

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,31 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared){
+        if(compared.squares < this.squares){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int priceDifference(Apartment compared){
+        int apartmentPrice = this.princePerSquare * this.squares;
+        int comparedPrice = compared.princePerSquare * compared.squares;
+        if(apartmentPrice - comparedPrice < 0){
+            return comparedPrice -apartmentPrice;
+        } else
+        return apartmentPrice - comparedPrice;
+    }
+
+    public boolean moreExpensiveThan(Apartment compared){
+        int apartmentMoreExpensive = this.princePerSquare * this.squares * this.rooms;
+        int compareApartmentMoreExpensive = compared.princePerSquare * compared.squares * compared.rooms;
+        if(apartmentMoreExpensive > compareApartmentMoreExpensive){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
