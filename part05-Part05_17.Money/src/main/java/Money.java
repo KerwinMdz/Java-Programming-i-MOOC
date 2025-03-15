faad50ece8433c555554c606c9ab4b4d2c0e308d
Kerwin <kerwinmondesir@outlook.com>
2025-03-14 22:09:53 -0300

    Completed Part05

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -30,6 +30,45 @@ public class Money {
         return newMoney;
     }
 
+    public boolean lessThan(Money compared){
+        if(this == compared){
+            return true;
+        }
+
+        if(!(compared instanceof Money)){
+            return false;
+        }
+
+        Money comparedMoney = (Money) compared;
+
+        if(this.euros < compared.euros){
+            return true;
+        }
+        if(this.euros == compared.euros){
+            if(cents < compared.cents){
+                return true;
+            }
+        }
+            return false;
+        
+    }
+    public Money minus(Money decreaser){
+        int totalEuros = this.euros - decreaser.euros;
+        int totalCents = this.cents - decreaser.cents;
+
+        if(totalEuros >= 0){
+            if(totalCents < 0){
+                totalEuros -= 1;
+                totalCents += 100;
+            }
+        } else {
+            totalEuros = 0;
+            totalCents = 0;
+        }
+        Money newMoneyObject = new Money(totalEuros, totalCents);
+        return newMoneyObject;
+    }
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
