5e982e73a8d95ff91a5cdd0fb74e237e704ac890
Kerwin <kerwinmondesir@outlook.com>
2025-04-06 22:29:05 -0300

    Completed Part06

--- part06-Part06_01.Menu/src/main/java/Menu.java
+++ part06-Part06_01.Menu/src/main/java/Menu.java
@@ -10,4 +10,24 @@ public class Menu {
     }
 
     // implement the required methods here
+    public void addMeal(String meal){
+            if(this.meals.contains(meal)){
+            
+            }else {
+                this.meals.add(meal);
+            }
+
+    }
+
+    //Print meals
+    public void printMeals(){
+        for(String meals : this.meals){
+            System.out.println(meals);
+        }
+    }
+
+    //Clearing menu
+    public void clearMenu(){
+        this.meals.clear();
+    }
 }
