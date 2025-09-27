70fbcd29cbe5e9a4158e198d8e9709b68f028e65
Kerwin <kerwinmondesir@outlook.com>
2025-09-27 01:54:29 -0300

    Completed Part 06

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -19,4 +19,27 @@ public class SimpleCollection {
         return this.elements;
     }
     
+@Override
+public String toString() {
+    if (this.getElements().isEmpty()) {
+        return "The collection " + this.name + " is empty.";
+    }
+
+    String result;
+    if (elements.size() == 1) {
+        result = "The collection " + this.name + " has 1 element:\n";
+    } else {
+        result = "The collection " + this.name + " has " + elements.size() + " elements:\n";
+    }
+
+    for (int i = 0; i < elements.size(); i++) {
+        result += elements.get(i);
+        if (i < elements.size() - 1) {
+            result += "\n";
+        }
+    }
+
+    return result;
+}
+
 }
